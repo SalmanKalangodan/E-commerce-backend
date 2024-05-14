4d525ffaef5896a5b86892388928189342df9de1
salman k <salman371k@gmail.com>
2024-05-15 01:06:51 +0530

    .

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ mongoose.connect(DB)
 .then(()=>console.log('db connect'))
 .catch((err)=>console.log(err))
 app.use(cros({
-    
+    origin : 'http://localhost:5173/',
+    credentials : true
 }))
 app.use(express.json())
 app.use('/api/user',Authrout)
