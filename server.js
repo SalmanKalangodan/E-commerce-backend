871d04f555c27042a1f1a5f4fb1ec8e0ac6a7bd5
salman k <salman371k@gmail.com>
2024-05-29 16:38:24 +0530

    .

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose.connect(DB)
 .then(()=>console.log('db connect'))
 .catch((err)=>console.log(err))
 app.use(cros({
-    origin : 'front-end-huou.vercel.app',
+    origin : 'https://front-end-huou.vercel.app',
     credentials : true
 }))
 app.use(express.json())
